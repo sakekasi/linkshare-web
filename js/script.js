42bf1f8958b4e1356b8ea63738fac07f44394d39
Saketh Kasibatla <sake.kasi@gmail.com>
2014-03-03 02:30:46 -0800

    updated webpage to provide basic interning functionality with notifications

--- js/script.js
+++ js/script.js
@@ -1,23 +1,87 @@
-$(document).ready(function() {
-    var templateData = [
-	{
-            id: 1,
-	    title: "Google",
-	    url: "http://www.google.com"
-	},
-	{
-	    id: 2,
-	    title: "Yahoo",
-	    url: "http://www.yahoo.com"
-	},
-	{
-	    id: 3,
-	    title: "Xkcd",
-	    url: "http://www.xkcd.com"
-	}
-    ]
+//var base="http://ec2-54-201-178-20.us-west-2.compute.amazonaws.com:8080/"
+var base="http://localhost:3000/"
 
-    templateData.map( function(item){
-	$('#links-form').append(ich.link(item));
+$(document).ready(function() {
+    $("#lsubmit").click(function(){
+	var title = $("input#title").val();
+	var url = $("input#url").val();
+	var subrequest = function(t, u) {
+	    $.ajax({
+		type: "POST",
+		contentType: "application/json",
+		dataType: "text",
+		url: base+"link",
+		data: JSON.stringify({title: t, url: u}),
+		success: function () {
+		    create_latest_link();
+		},
+		error: xhr_error
+	    });
+	    $.notify("interned link", "success");
+	};
+
+	if( url == "" ){
+	    console.log("empty");
+	} else {
+	    reset_form($(this));
+	    if( title == "" ){
+		$.ajax({
+		    type: "GET",
+		    dataType: "text",
+		    url: base+"lookup",
+		    data: {url: encodeURIComponent(url)},
+		    success: function(t){
+			subrequest(t, url);
+		    },
+		    error: xhr_error
+		});
+		$.notify("checking title", "info");
+	    } else {
+		subrequest(title, url);
+	    }
+	}
     });
+
+
+    //fetch first page of links from the server.
+    $.ajax({
+	dataType: "json",
+	url: base+"links",
+	data: "",
+	crossDomain: true,
+	success: function (data) {
+	    $.each( data.links, function(i, item){
+		create_link(item);
+	    });
+	},
+	error: xhr_error
+    });
+    $.notify("fetching links", "info");
 });
+
+function create_latest_link(){
+    $.ajax({
+	dataType: "json",
+	url: base+"link",
+	data: "",
+	crossDomain: true,
+	success: function (data) {
+	    create_link(data);
+	},
+	error: xhr_error
+    });
+}
+
+function create_link(link){
+    $(ich.link(link, true)).hide().appendTo('#links-form').fadeIn(600);
+}
+
+function reset_form(t){
+    t.closest('form').find("input[type=text], textarea").val("");
+}
+
+function xhr_error(xhr, status, err) {
+    $.notify(err);
+    console.log(xhr, status, err);
+}
+
